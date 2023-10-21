8c628380438b9a3d9e97fbc5cdfe9e7a4acdb051
Akash P <47393739+FrostyCake47@users.noreply.github.com>
2023-10-21 20:50:04 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
 import flet as ft
+from pytube import YouTube
+from pytube import Playlist
+from pytube import Search
+import os
+
+folder_dir = os.path.dirname(__file__)
+
+def download(URL = f"https://www.youtube.com/watch?v=dQw4w9WgXcQ", video_format="MP3"):
+    video = YouTube(URL)
+    available_itags = [stream.itag for stream in video.streams]
+    print("Available itags:", available_itags)
+
+    if video_format == "MP4":
+        st = video.streams.get_by_itag(22)
+        print("downloading video")
+    else:
+        st = video.streams.get_by_itag(249)
+        print("downloading audio")
+
+    st.download(output_path=folder_dir)
+
+
 
 def main(page):
     def btn_click(e):
@@ -6,11 +28,13 @@ def main(page):
             txt_name.error_text = "Enter the URL"
             page.update()
         else:
+
             url = txt_name.value
-            page.clean()
-            page.add(ft.Text(f"{url}"))
+            page.add(ft.Text(f"video is been downloaded"))
+            #download(URL=url)
 
     txt_name = ft.TextField(label="Enter the URL")
+    
 
     page.add(txt_name, ft.ElevatedButton("Submit", on_click=btn_click))
 
